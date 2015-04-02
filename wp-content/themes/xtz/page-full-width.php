2f332de2a62dc2f7680d22d18fb6d607b8dcc62c
mstarniciuc <mstarniciuc@mstarniciuc-System-Product-Name>
2015-04-02 12:30:46 +0300

    full site in two languages

--- wp-content/themes/xtz/page-full-width.php
+++ wp-content/themes/xtz/page-full-width.php
@@ -19,38 +19,40 @@ get_header(); ?>
         </div><!-- #primary -->
     </main><!-- #main -->
         <?php
-        
+    
+	$l = get_bloginfo("language");
+	$lang = ($l == 'ru-RU')?"_rus":"";	
+		
 	if ( (function_exists( 'of_get_option' )) && (of_get_option('slidetitle5',true) !=1) ) {
-	if ( of_get_option('slider_enabled') != 0 )  
+	if ( of_get_option('slider_enabled'.$lang) != 0 )  
 		{ ?>
 	<div class="slider-wrapper theme-default"> 
     	<div class="ribbon"></div>    
     		<div id="slider" class="nivoSlider">
     			<?php
 		  		$slider_flag = false;
-		  		for ($i=1;$i<6;$i++) {
-		  			$caption = ((of_get_option('slidetitle'.$i, true)=="")?"":"#caption_".$i);
-					if ( of_get_option('slide'.$i, true) != "" ) {
-						echo "<div class='slide'><a href='".of_get_option('slideurl'.$i, true)."'><img src='".of_get_option('slide'.$i, true)."' title='".$caption."'></a></div>"; 
-						$slider_flag = true;
+		  		for ($i = 1; $i < 10; $i++) {
+						$caption = ((of_get_option('slidetitle' . $i.$lang, true) == "") ? "" : "#caption_" . $i);
+						if (of_get_option('slide' . $i.$lang, true) != "") {
+							echo "<a href='" . esc_url(of_get_option('slideurl' . $i.$lang, true)) . "'><img src='" . of_get_option('slide' . $i.$lang, true) . "' title='" . $caption . "'></a>";
+							$slider_flag = true;
+						}
 					}
-				}
 				?>  
     		</div><!--#slider-->
-    		<?php for ($i=1;$i<6;$i++) {
-    				$caption = ((of_get_option('slidetitle'.$i, true)=="")?"":"#caption_".$i);
-    				if ($caption != "")
-    				{
-	    				 echo "<div id='caption_" . $i . "' class='nivo-html-caption'>"
-                                        . "<div class='nivocapper'>";
-                                echo "<div class='slide-title'>" . of_get_option('slidetitle' . $i, true) . "</div>";
-                                echo "<div class='slide-description'>" . of_get_option('slidedesc' . $i, true) . "</div>";
-                                echo "<a href='" . esc_url(of_get_option('slideurl' . $i, true)) . "' class='btn btn-slide'>Citește mai mult</a>";
-                                echo "</div></div>";
-    				}
-    			}	
-    	    
-			?>
+    		<?php
+				for ($i = 1; $i < 10; $i++) {
+					$caption = ((of_get_option('slidetitle' . $i.$lang, true) == "") ? "" : "#caption_" . $i);
+					if ($caption != "") {
+						echo "<div id='caption_" . $i . "' class='nivo-html-caption'>"
+						. "<div class='nivocapper'>";
+						echo "<div class='slide-title'>" . of_get_option('slidetitle' . $i.$lang, true) . "</div>";
+						echo "<div class='slide-description'>" . of_get_option('slidedesc' . $i.$lang, true) . "</div>";
+						echo "<a href='" . esc_url(of_get_option('slideurl' . $i.$lang, true)) . "' class='btn btn-slide'>".of_get_option('more'.$lang, true)."</a>";
+						echo "</div></div>";
+					}
+				}
+				?>
     </div>	
 	<?php 
 			}
